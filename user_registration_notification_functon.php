805f05f44238727061a2a9a948cbac40342c655b
wpself <contact@wpself.com>
2014-08-19 21:23:27 +0530

    Create user_registration_notification_functon.php

---
+++ user_registration_notification_functon.php
@@ -0,0 +1,31 @@
+/*
+ *   User Registration Notification function
+ */
+if ( !function_exists('rxtheme_user notification') ) {
+    function rxtheme_user_notification( $user_id, $plaintext_pass = '' ) {
+        $user = new WP_User($user_id);
+
+        $user_login = stripslashes($user->user_login);
+        $user_email = stripslashes($user->user_email);
+
+        $message  = sprintf(__('New user registration on your blog %s:'), get_option('blogname')) . "\r\n\r\n";
+        $message .= sprintf(__('Username: %s'), $user_login) . "\r\n\r\n";
+        $message .= sprintf(__('E-mail: %s'), $user_email) . "\r\n";
+
+        @wp_mail(get_option('admin_email'), sprintf(__('[%s] New User Registration'), get_option('blogname')), $message);
+
+        if ( empty($plaintext_pass) )
+            return;
+
+        $message  = __('Hi there,') . "\r\n\r\n";
+        $message .= sprintf(__("Welcome to %s! Here's how to log in:"), get_option('blogname')) . "\r\n\r\n";
+        $message .= wp_login_url() . "\r\n";
+        $message .= sprintf(__('Username: %s'), $user_login) . "\r\n";
+        $message .= sprintf(__('Password: %s'), $plaintext_pass) . "\r\n\r\n";
+        $message .= sprintf(__('If you have any problems, please contact me at %s.'), get_option('admin_email')) . "\r\n\r\n";
+        $message .= __('Adios!');
+
+        wp_mail($user_email, sprintf(__('[%s] Your username and password'), get_option('blogname')), $message);
+
+    }
+}
